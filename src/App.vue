263aefaedbcd2499516f4304326961a1afcb52e4
TheKinng96 <fengyuan.yap@example.com>
2020-09-26 15:31:39 +0900

    added basic components

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <stored-resources :resources="storedResources" />
+    <button>Enter</button>
+  </div>
+</template>
+
+<script>
+import StoredResources from './components/Resources/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        },
+        {
+          id: 'youtube',
+          title: 'Youtube',
+          description: 'Learn from videos, use Youtube.',
+          link: 'https://youtube.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Resources/StoredResources.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <ResourceItem
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem';
+
+export default {
+  props: ['resources'],
+  components: {
+    ResourceItem
+  }
+};
+</script>
